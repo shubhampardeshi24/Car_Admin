995b89b7dad93dd6631028ff6b9590f976cc0fae
shubhampardeshi24 <pardeshishubham143@gmail.com>
2022-01-28 15:08:58 +0530

    First Commit

---
+++ src/main/java/com/car/CarSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package com.car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSearchEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarSearchEngineApplication.class, args);
+	}
+
+}
